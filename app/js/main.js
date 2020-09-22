6109acaed442024482ac64c4e827bc27cc5a7427
Vitalie Perdeleanu <vperdeleanu@gmail.com>
2020-09-22 15:35:38 +0300

    header+slider

---
+++ app/js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+    
+    $('.banner-section__slider').slick({
+        dots: true,
+        prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="img/left.svg" alt=""></button>',
+        nextArrow:'<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="img/right.svg" alt=""></button>'
+    })      
+    
+
+  
+});
