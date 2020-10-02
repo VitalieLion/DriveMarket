7a499bdd44063d5aab66e8684a2997f4d3198b5f
Vitalie Perdeleanu <vperdeleanu@gmail.com>
2020-10-02 17:53:42 +0300

    tabs&slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,11 +1,34 @@
+
+
 $(function () {
     
     $('.banner-section__slider').slick({
         dots: true,
         prevArrow: '<button class="banner-section__slider-btn banner-section__slider-btnprev"><img src="img/left.svg" alt=""></button>',
-        nextArrow:'<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="img/right.svg" alt=""></button>'
-    })      
+        nextArrow: '<button class="banner-section__slider-btn banner-section__slider-btnnext"><img src="img/right.svg" alt=""></button>'
+    });
     
+    $('.tab').on('click', function (e) {
+        e.preventDefault();
+    
+        $('.tab').removeClass('tab--active');
+        $('.tabs__content').removeClass('tabs-content--active');
+    
+        $(this).addClass('tab--active');
+        $($(this).attr('href')).addClass('tabs-content--active');
 
-  
+
+    });
+    $('.product__item-favorite').on('click', function () {
+        $(this).toggleClass('product__item-favorite--active')
+    });
+
+    $('.product-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        prevArrow: '<button class="product-slider__slider-btn product-slider__slider-btnprev"><img src="img/left-black.svg" alt=""></button>',
+        nextArrow: '<button class="product-slider__slider-btn product-slider__slider-btnnext"><img src="img/right-black.svg" alt=""></button>'
+    });
+
+
 });
